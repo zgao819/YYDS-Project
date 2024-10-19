b34da3bb1c53009983ba99a05e53b8824bd7607b
zgao819 <zgao819@aucklanduni.ac.nz>
2024-10-20 05:37:04 +1300

    Reapply "fixed rote"
    This reverts commit d4e16ad2a85b95684105319a6cbffc525235cfec.

--- project/js/alcohol.js
+++ project/js/alcohol.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function () {
         goBackBtn.setAttribute('href', 'result2.html');
     } else {
 
-        goBackBtn.setAttribute('href', 'index.html');
+        goBackBtn.setAttribute('href', 'result2.html');
     }
 });
 
